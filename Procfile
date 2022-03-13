e1a1dabac1fdfa41ef289460e777d2f427e5c657
unknown <you@domain.com>
2022-03-13 23:16:19 +0600

    Second Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT cWork_microjob.wsgi:application
--- cWork_microjob/settings.py
+++ cWork_microjob/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$lybsbi3mhba4^&(^$ayv=0q1_qk&y8rtvfu(6u^y@7c4x+a6l'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cwork123.herokuapp.com","localhost"]
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.security.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-   
-]
+STATIC_ROOT = BASE_DIR / 'static',
 
 
  
